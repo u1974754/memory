fd0e65eb3945bef7df5a5202508404f5722cc211
u1974754 <u1974754@campus.udg.edu>
2022-04-23 19:16:28 +0200

    Update game.js

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -24,22 +24,27 @@ class GameScene extends Phaser.Scene {
 		var cartas = options_data.cards*2;
 		var vector_cartas = arraycards.slice(0,cartas);
 		this.cameras.main.setBackgroundColor(0xBFFCFF);
-		
+
+		if (options_data.cards=2) {
 		this.add.image(250, 300, arraycards[0]);
 		this.add.image(350, 300, arraycards[1]);
 		this.add.image(450, 300, arraycards[2]);
 		this.add.image(550, 300, arraycards[3]);
-		
-		this.cards = this.physics.add.staticGroup();
-		
-		if (options_data.cards=2) {
 
 		this.cards.create(250, 300, 'back');
 		this.cards.create(350, 300, 'back');
 		this.cards.create(450, 300, 'back');
 		this.cards.create(550, 300, 'back');
 	}
-		if  (options_data.cards=3) {
+		if (options_data.cards=3) {
+
+		this.add.image(250, 300, arraycards[0]);
+		this.add.image(350, 300, arraycards[1]);
+		this.add.image(450, 300, arraycards[2]);
+		this.add.image(550, 300, arraycards[3]);
+		this.add.image(550, 300, arraycards[4]);
+		this.add.image(550, 300, arraycards[5]);
+
 		this.cards.create(250, 300, 'back');
 		this.cards.create(350, 300, 'back');
 		this.cards.create(450, 300, 'back');
@@ -47,6 +52,13 @@ class GameScene extends Phaser.Scene {
 		this.cards.create(650, 300, 'back');
 		this.cards.create(750, 300, 'back');
 	}
+		
+		this.cards = this.physics.add.staticGroup();
+		
+		
+
+	
+		
 		
 		let i = 0;
 		this.cards.children.iterate((card)=>{
