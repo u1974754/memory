2c5374cbc41512144eba1c870c0c01fcabe337f7
u1974754 <u1974754@campus.udg.edu>
2022-04-23 20:04:46 +0200

    Update game.js

--- js/scenes/game.js
+++ js/scenes/game.js
@@ -25,20 +25,30 @@ class GameScene extends Phaser.Scene {
 		this.vector_cartas = arraycards.slice(0,cartas);
 		this.cameras.main.setBackgroundColor(0xBFFCFF);
 
+		this.cards = this.physics.add.staticGroup();
+		for(var k = 0; k < cartas; k++){
+
+            this.add.image(125*k+50,300,cartasvector[k]);
+			
+            this.cards.create(125*k+50,300,'back');
+
+        }
 		
+
+		/*
 		this.add.image(250, 300, arraycards[0]);
 		this.add.image(350, 300, arraycards[1]);
 		this.add.image(450, 300, arraycards[2]);
 		this.add.image(550, 300, arraycards[3]);
 
 		this.cards = this.physics.add.staticGroup();
-		
+
 		this.cards.create(250, 300, 'back');
 		this.cards.create(350, 300, 'back');
 		this.cards.create(450, 300, 'back');
 		this.cards.create(550, 300, 'back');
 		
-		
+		*/
 		
 		let i = 0;
 		this.cards.children.iterate((card)=>{
